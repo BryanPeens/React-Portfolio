174d60afbcea1ff07c943d0b1bbff66f2f98eba1
Bryan Peens <peensbryan1@gmail.com>
2022-09-02 03:24:50 +0200

    updated site styling and responsive scroll

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
+import workImg1 from '../assets/workImg1.png';
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] py-[120px]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-800'>
@@ -17,20 +18,20 @@ const Work = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
           {/* Grid Item 1*/}
-          <div style={{ backgroundImage: `url(${realEstate})` }}
+          <div style={{ backgroundImage: `url(${workImg1})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                React Weather App
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://bryanpeens.github.io/weather-app-react' target='_blank'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/BryanPeens/weather-app-react' target='_blank'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
